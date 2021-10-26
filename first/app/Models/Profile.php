d4bc04f3ed33588c2d9e232e175d0531aaa62c44
Nikolay <npenchev19@gmail.com>
2021-10-26 23:38:43 +0300

    all files

---
+++ first/app/Models/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+   // use HasFactory;
+protected $guarded =[];
+
+    public function profileImage(){
+        $imagePath = ($this->image) ? $this->image : 'uploads/CcmzUOo5UNhC5cPrOvZNaFextSqN8G0j1Uk2WRrc.jpg';
+    return '/storage/'.$imagePath;
+
+
+
+    }
+
+    public function user()
+    {
+
+        return $this->belongsTo(User::class);
+
+
+    }
+}
